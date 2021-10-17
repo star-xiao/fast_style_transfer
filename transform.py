425846829c60fd667b33fd00e25a5c569872917e
Xiao <593072042@qq.com>
2021-10-17 18:01:53 +0800

    commit 2021101705

--- transform.py
+++ transform.py
@@ -5,7 +5,7 @@ from trans_net import feed_forward
 from utils import load_img, save_img
 import argparse
 
-CONTENT_IMAGE = './content/DSC01716.jpg'
+CONTENT_IMAGE = './content/great wall.jpg'
 WEIGHTS_PATH = './weights/wave/weights_wave'
 start = CONTENT_IMAGE.rindex('/')
 OUTPUT_NAME = CONTENT_IMAGE[start+1:]
