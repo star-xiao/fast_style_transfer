73a3ef8519c40576d535b061c07eb38cf8fecd0f
Xiao <593072042@qq.com>
2021-10-30 15:08:36 +0800

    commit 2021103008

--- transform.py
+++ transform.py
@@ -6,7 +6,7 @@ from utils import load_img, save_img
 import argparse
 
 CONTENT_IMAGE = './content/DSC02809.jpg'
-WEIGHTS_PATH = './weights/starry night/weights_starry night'
+WEIGHTS_PATH = './weights/starry night/weights'
 start = CONTENT_IMAGE.rindex('/')
 OUTPUT_NAME = CONTENT_IMAGE[start+1:]
 OUTPUT_NAME = os.path.join('/kaggle/working/', OUTPUT_NAME)
